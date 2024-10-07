4f5af2da02173365b90666df92fbdb8d8ea6e925
Your Name <you@example.com>
2024-10-07 10:41:22 +0530

    First commit

---
+++ Frontend/Javascript/class-1/live/index.js
@@ -0,0 +1,16 @@
+console.log("hey i am Biswajeet")
+var age='30';
+console.log(age)
+
+var a=30;
+var b=60;
+sum=(a+b)
+console.log(sum)
+
+console.log(addr);
+
+var addr="bbsr";
+
+console.log(addr);
+
+console.log(typeof null)
---
+++ Frontend/Javascript/class-1/live/notes.md
@@ -0,0 +1,9 @@
+## how to push in Github
+
+- step-1 : `git add.`
+- step-2 : `git commit -m "your commit message" `
+- step-3 : `git push origin`
+
+if you want to pull git pull.
+
+git pull
