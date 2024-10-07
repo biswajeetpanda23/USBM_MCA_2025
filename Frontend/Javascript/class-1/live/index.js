c1fa6e9ea29403197f4fd77c3935205dec1cb499
biswajeet panda <pandabiswajeet5@gmail.com>
2024-10-07 11:54:30 +0530

    Use of Biotics

--- Frontend/Javascript/class-1/live/index.js
+++ Frontend/Javascript/class-1/live/index.js
@@ -1,16 +1,63 @@
-console.log("hey i am Biswajeet")
-var age='30';
-console.log(age)
+// console.log("hey i am Biswajeet")
+// var age='30';
+// console.log(age)
 
-var a=30;
-var b=60;
-sum=(a+b)
-console.log(sum)
+// var a=30;
+// var b=60;
+// sum=(a+b)
+// console.log(sum)
 
-console.log(addr);
+// console.log(addr);
 
-var addr="bbsr";
+// var addr="bbsr";
 
-console.log(addr);
+// console.log(addr);
 
-console.log(typeof null)
+// console.log(typeof null)
+
+let data;
+console.log(typeof data);
+console.log(data);
+
+//number data type
+
+let number1=50;
+let desc_no=1.003;
+let pi=3.14;
+let  age = 25;
+
+
+console.log(typeof number1);
+console.log(typeof pi);
+
+
+// addition
+
+let num1=100;
+let num2=200;
+let add=num1+num2;
+console.log(add);
+
+let num3=100;
+let num4=200;
+let sub=num3-num4;
+console.log(sub);
+
+
+let num5=100;
+let num6=200;
+let mul=num5*num6;
+console.log(mul);
+
+
+let num7=100;
+let num8=200;
+let div=num8/num7;
+console.log(div);
+
+let num9=100;
+let num10=200;
+let mod=num9%num10;
+console.log(mod);
+
+
---
+++ Frontend/Javascript/class-2/index.js
@@ -0,0 +1,39 @@
+
+//symbols
+
+
+let sym1=Symbol("sym1");
+let sym2=Symbol("sym1");
+
+
+
+//NaN
+
+let num11=0;
+let num12=10;
+
+console.log(num11/num12);
+
+
+//boolean
+
+let isStudent=false;
+let isTeacher=true;
+
+console.log(typeof isStudent);
+console.log(typeof isTeacher);
+
+console.log(isStudent);
+console.log(isTeacher);
+
+//string
+
+let name='raja';
+let city='jajpur';
+let age=20;
+let roll=26;
+let clg='usbm';
+let branch='mca'
+
+
+console.log(`my name is${name} living ${city}  i am ${age}years old and roll no is${roll} at${clg} in ${branch}`);
